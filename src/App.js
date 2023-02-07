227cfcf0dab09d3ddb215ec80a5a26e62c70e87d
Sergey <wer113wer@yandex.ru>
2023-02-07 20:33:05 +0300

    Wrote a template for routing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import { AppBar, Box, Container, Icon, IconButton, Toolbar, Typography } from '@
 import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react';
 import { Menu } from '@mui/icons-material';
+import MainPage from './Components/MainPage';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const pages = ["Products", "Pricing", "Blog"];
 
 function App() {
   
   return (  
+     <Router>
+      <div className="App"> 
+      <CoolAppBar></CoolAppBar>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+      </Routes>
+      </div>
+     </Router>
+
+
     
-    <CoolAppBar></CoolAppBar>
   );
 }
 
