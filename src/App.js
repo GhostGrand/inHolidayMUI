d39ebed8a159a72b71f8880258297919b9b8ebcf
Sergey <wer113wer@yandex.ru>
2023-02-07 18:16:54 +0300

    Project created. Header written

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import CoolAppBar from './Components/CoolAppBar'
+import { AppBar, Box, Container, Icon, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+import { Menu } from '@mui/icons-material';
+
+const pages = ["Products", "Pricing", "Blog"];
+
+function App() {
+  
+  return (  
+    
+    <CoolAppBar></CoolAppBar>
+  );
+}
+
+export default App;
