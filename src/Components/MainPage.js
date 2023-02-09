8a1f54ba777e74e15ea09385621aec5f0d7cbfc3
Sergey <wer113wer@yandex.ru>
2023-02-09 23:38:56 +0300

    Created a home page (stub)
    Started writing styles for the home page

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,7 +1,27 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
+import { mainPageStyles } from './Styles/styles'
+
 
 export default function MainPage() {
+  
+// const classes = useStyles();
+  
+
   return (
-    <div>MainPage</div>
+    <Container sx={mainPageStyles.container}>
+      <Box sx={mainPageStyles.container}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={6} md={4}>
+            <Typography variant="h4" sx={mainPageStyles.typography}>
+              Лучшие приглашения для лучших клиентов! Работаем с вами.
+            </Typography>
+          </Grid>
+          <Grid item xs={6} md={8} sx={mainPageStyles.gridImage}>
+            <img src={require('./Content/sales.png')} alt="Sale" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   )
 }
---
+++ src/Components/Styles/styles.js
@@ -0,0 +1,20 @@
+// Styles/styles.js
+export const mainPageStyles = {
+  container: {
+    flexGrow: 1
+  },
+  typography: {
+    // paddingTop: 8,
+    fontWeight: 700,
+    textAlign: 'left'
+  },
+  gridImage: {
+    display: { xs: 'none', md: 'block' },
+    width: '100vw',
+    left: '49%',
+    transform: 'translateX(-48.8%)',
+    position: 'relative'
+  }
+}
+
+
